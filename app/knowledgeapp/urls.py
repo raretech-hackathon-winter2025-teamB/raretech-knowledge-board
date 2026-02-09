385fd5d3abf492f8f124961ab15a92385fb96c2a
anazawa <anazawa.71114@gmail.com>
2026-02-10 00:16:30 +0900

    削除機能再修正

--- app/knowledgeapp/urls.py
+++ app/knowledgeapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.TopView.as_view(), name='top'),
     path('home/', views.QuestionList.as_view(), name='question'),
     path('question/new/', views.QuestionCreate.as_view(), name='new_question'),
+    path('delete/<int:pk>', views.QuestionDelete.as_view(), name='delete_question')
 ]
