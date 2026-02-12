e4283c10e04552b630d6cc111b7020a92e8b2cbe
anazawa <anazawa.71114@gmail.com>
2026-02-12 22:42:26 +0900

    質問削除機能と更新機能の追加

--- app/knowledgeapp/urls.py
+++ app/knowledgeapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.TopView.as_view(), name='top'),
     path('home/', views.QuestionList.as_view(), name='question'),
     path('question/new/', views.QuestionCreate.as_view(), name='new_question'),
+    path('delete_question/<int:pk>/', views.delete_question, name='delete_question'),
+    path('question/<int:pk>/', views.QuestionUpdate.as_view(), name='update_question'),
 ]
